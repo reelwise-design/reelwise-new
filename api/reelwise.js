742246dc1e3c8073eed28e8e07b1cc1a62d5d418
reelwise-design <djcohen@hotmail.com>
2026-09-13 13:55:49 -0400

    Add new movie entries to reelwise data

--- api/reelwise.js
+++ api/reelwise.js
@@ -1,4 +1,5 @@
 const reelwiseData = {
+
   744: {
     title: "Top Gun",
 
@@ -24,6 +25,65 @@ const reelwiseData = {
       "Talk to me, Goose."
     ]
   },
+
+
+  1366: {
+    title: "Rocky",
+
+    facts: [
+      "Rocky is as much a love story and character drama as it is a boxing movie.",
+      "The story centers on Rocky Balboa getting an unlikely shot at heavyweight champion Apollo Creed.",
+      "The ending became unusual for a sports movie because Rocky's emotional victory matters more than the official result of the fight."
+    ],
+
+    casting: [
+      "Sylvester Stallone wrote Rocky and insisted on playing Rocky Balboa himself.",
+      "Carl Weathers played Apollo Creed, beginning one of the most important relationships in the Rocky series.",
+      "Burgess Meredith became unforgettable as Rocky's trainer Mickey Goldmill."
+    ],
+
+    soundtrack: [
+      "Bill Conti composed the film's score.",
+      "Gonna Fly Now became one of the most recognizable training themes in movie history.",
+      "The music helped turn Rocky's Philadelphia training scenes into enduring pop-culture images."
+    ],
+
+    quotes: [
+      "Yo, Adrian!",
+      "It ain't about how hard you hit.",
+      "I just wanna go the distance."
+    ]
+  },
+
+
+  1367: {
+    title: "Rocky II",
+
+    facts: [
+      "Rocky II begins almost immediately after the championship fight at the end of Rocky.",
+      "The sequel gives Apollo Creed a much larger emotional stake in proving the first fight was not a fluke.",
+      "Rocky's rematch with Apollo became one of the defining fights of the franchise."
+    ],
+
+    casting: [
+      "Sylvester Stallone returned as writer, director and star.",
+      "Carl Weathers returned as Apollo Creed with a more aggressive and determined version of the character.",
+      "Talia Shire's Adrian remains central to Rocky's motivation throughout the film."
+    ],
+
+    soundtrack: [
+      "Bill Conti returned to score Rocky II.",
+      "The familiar Rocky musical themes were expanded for the sequel's larger training and fight sequences.",
+      "The music again builds toward Rocky's transformation from struggling fighter to championship contender."
+    ],
+
+    quotes: [
+      "Yo, Adrian! I did it!",
+      "Win.",
+      "You're gonna eat lightning and you're gonna crap thunder!"
+    ]
+  },
+
 
   1371: {
     title: "Rocky III",
@@ -51,10 +111,187 @@ const reelwiseData = {
       "I pity the fool.",
       "You ain't so bad."
     ]
+  },
+
+
+  1374: {
+    title: "Rocky IV",
+
+    facts: [
+      "Rocky IV places the Rocky story against the Cold War backdrop of the United States and Soviet Union.",
+      "Apollo Creed's exhibition fight with Ivan Drago completely changes the direction of the story.",
+      "The film contains one of the franchise's most famous training contrasts: Rocky in the snow and Drago in a high-tech laboratory."
+    ],
+
+    casting: [
+      "Dolph Lundgren became internationally famous playing Soviet boxer Ivan Drago.",
+      "Carl Weathers returned as Apollo Creed in one of the character's most important appearances.",
+      "Brigitte Nielsen played Ludmilla Drago alongside Lundgren's Ivan Drago."
+    ],
+
+    soundtrack: [
+      "Burning Heart by Survivor became one of the movie's signature songs.",
+      "Hearts on Fire accompanies Rocky's famous Soviet Union training montage.",
+      "The soundtrack is a major part of Rocky IV's fast-paced, music-driven style."
+    ],
+
+    quotes: [
+      "If he dies, he dies.",
+      "I must break you.",
+      "No pain!"
+    ]
+  },
+
+
+  881: {
+    title: "A Few Good Men",
+
+    facts: [
+      "A Few Good Men began as a stage play written by Aaron Sorkin before becoming a feature film.",
+      "The story centers on a military court-martial involving two Marines and an alleged Code Red.",
+      "The courtroom confrontation between Kaffee and Colonel Jessup became one of the most famous scenes in modern legal dramas."
+    ],
+
+    casting: [
+      "Tom Cruise stars as Navy lawyer Daniel Kaffee.",
+      "Jack Nicholson plays Colonel Nathan Jessup in one of the film's most memorable performances.",
+      "Demi Moore, Kevin Bacon, Kevin Pollak and Kiefer Sutherland are part of the ensemble cast."
+    ],
+
+    soundtrack: [
+      "Marc Shaiman composed the film's score.",
+      "The music is restrained compared with the movie's dialogue-heavy courtroom scenes.",
+      "Much of the film's intensity comes from the performances and escalating legal confrontation rather than action."
+    ],
+
+    quotes: [
+      "You can't handle the truth!",
+      "I want the truth!",
+      "You want answers?"
+    ]
+  },
+
+
+  14534: {
+    title: "Rudy",
+
+    facts: [
+      "Rudy is based on the story of Daniel Ruettiger and his determination to play football at Notre Dame.",
+      "The movie became one of the best-known underdog sports films of the 1990s.",
+      "Its emotional climax centers on Rudy finally getting onto the field for Notre Dame."
+    ],
+
+    casting: [
+      "Sean Astin plays Rudy Ruettiger.",
+      "Jon Favreau appears as Rudy's friend D-Bob years before becoming known as a major director and producer.",
+      "Vince Vaughn appears in an early film role as Notre Dame player Jamie O'Hara."
+    ],
+
+    soundtrack: [
+      "Jerry Goldsmith composed the score for Rudy.",
+      "The main theme became strongly associated with inspirational sports moments.",
+      "The score helps give the final football sequence much of its emotional impact."
+    ],
+
+    quotes: [
+      "I've been ready for this my whole life.",
+      "You're five foot nothin', a hundred and nothin'.",
+      "Having dreams is what makes life tolerable."
+    ]
+  },
+
+
+  11635: {
+    title: "Old School",
+
+    facts: [
+      "Old School became one of the defining studio comedies of the early 2000s.",
+      "The story follows three adult friends who accidentally create a fraternity while trying to reclaim part of their youth.",
+      "Frank's transformation into Frank the Tank became one of the movie's most memorable running jokes."
+    ],
+
+    casting: [
+      "Luke Wilson, Will Ferrell and Vince Vaughn form the movie's central trio.",
+      "Will Ferrell's performance as Frank became one of the roles that helped establish him as a major comedy star.",
+      "The supporting cast includes Jeremy Piven, Ellen Pompeo, Juliette Lewis and Seann William Scott."
+    ],
+
+    soundtrack: [
+      "The movie uses a mix of classic rock and party music to reinforce its college-comedy atmosphere.",
+      "Music is especially important during the party and fraternity scenes.",
+      "The soundtrack helps give the film its early-2000s comedy identity."
+    ],
+
+    quotes: [
+      "We're going streaking!",
+      "You're my boy, Blue!",
+      "Once it hits your lips, it's so good."
+    ]
+  },
+
+
+  18785: {
+    title: "The Hangover",
+
+    facts: [
+      "The Hangover follows three friends trying to reconstruct a missing night in Las Vegas after a bachelor party.",
+      "The mystery structure lets the audience discover the previous night's events at the same time as the characters.",
+      "The film became one of the biggest comedy hits of 2009 and launched a trilogy."
+    ],
+
+    casting: [
+      "Bradley Cooper, Ed Helms and Zach Galifianakis lead the central group.",
+      "Ken Jeong's Mr. Chow became one of the franchise's breakout characters.",
+      "Zach Galifianakis became a much bigger mainstream star after playing Alan."
+    ],
+
+    soundtrack: [
+      "The movie's soundtrack mixes rock, pop and Las Vegas-style music.",
+      "Music is used throughout the film to heighten the contrast between the glamorous Vegas setting and the group's disastrous night.",
+      "The end-credit sequence uses photographs to reveal what actually happened during the missing hours."
+    ],
+
+    quotes: [
+      "What happens in Vegas stays in Vegas.",
+      "I am a one-man wolf pack.",
+      "Paging Dr. Faggot."
+    ]
+  },
+
+
+  274167: {
+    title: "Daddy's Home",
+
+    facts: [
+      "Daddy's Home builds its comedy around a rivalry between a mild-mannered stepfather and the children's biological father.",
+      "The movie turns ordinary parenting situations into escalating competitions between Brad and Dusty.",
+      "The film was successful enough to lead to Daddy's Home 2."
+    ],
+
+    casting: [
+      "Will Ferrell plays Brad Whitaker, the devoted stepfather.",
+      "Mark Wahlberg plays Dusty Mayron, the charismatic biological father.",
+      "The movie reunited Ferrell and Wahlberg after they previously starred together in The Other Guys."
+    ],
+
+    soundtrack: [
+      "The soundtrack uses upbeat pop and rock music to support the movie's broad family-comedy tone.",
+      "Music is often used during the increasingly ridiculous competitions between Brad and Dusty.",
+      "The film's musical choices help contrast Brad's safe personality with Dusty's more rebellious image."
+    ],
+
+    quotes: [
+      "I'm the dad.",
+      "He's the father.",
+      "Did you touch my motorcycle?"
+    ]
   }
+
 };
 
+
 export default function handler(req, res) {
+
   const id = Number(req.query.id);
 
   if (!id) {
@@ -75,4 +312,5 @@ export default function handler(req, res) {
     available: true,
     ...data
   });
+
 }
