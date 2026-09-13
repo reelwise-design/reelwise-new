d3f25a3657f34ff6fd624132012e498bd9abd4e7
reelwise-design <djcohen@hotmail.com>
2026-09-13 11:43:59 -0400

    Update facts and casting details for Top Gun

--- reelwise.js
+++ reelwise.js
@@ -1,19 +1,23 @@
 const reelwiseData = {
   744: {
     title: "Top Gun",
+
     facts: [
-      "Tom Cruise's Maverick became one of the defining movie characters of the 1980s.",
-      "The movie's enormous popularity helped turn Top Gun into a cultural phenomenon far beyond the theater.",
-      "The chemistry and rivalry between Maverick and Iceman became one of the movie's most memorable relationships."
+      "Top Gun helped define the look and attitude of blockbuster movies in the 1980s.",
+      "Maverick and Iceman's rivalry became one of the film's most memorable relationships.",
+      "The movie turned naval aviation into a major part of pop culture."
     ],
+
     casting: [
-      "Top Gun helped cement Tom Cruise's status as one of Hollywood's biggest stars.",
-      "Val Kilmer's Iceman became such an iconic part of the movie that his character returned decades later in Top Gun: Maverick."
+      "Top Gun helped cement Tom Cruise as one of Hollywood's biggest stars.",
+      "Val Kilmer's Iceman became such an important part of the movie that the character returned decades later in Top Gun: Maverick."
     ],
+
     soundtrack: [
       "Danger Zone, performed by Kenny Loggins, became inseparable from Top Gun.",
       "Take My Breath Away, performed by Berlin, became another signature song from the film and won the Academy Award for Best Original Song."
     ],
+
     quotes: [
       "I feel the need... the need for speed!",
       "You can be my wingman any time.",
