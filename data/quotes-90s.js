bd9662784b21921178aa2c892fa6da280279f5eb
reelwise-design <djcohen@hotmail.com>
2026-09-17 14:22:08 -0400

    Add 1990s iconic quotes to quotes-90s.js

---
+++ data/quotes-90s.js
@@ -0,0 +1,157 @@
+/*
+  ============================================================
+  REELWISE ICONIC QUOTE VAULT — 1990s
+  ============================================================
+*/
+
+const QUOTES_90S = {
+
+  "home alone": [
+    "Keep the change, ya filthy animal.",
+    "This is my house. I have to defend it."
+  ],
+
+  "the silence of the lambs": [
+    "Hello, Clarice.",
+    "A census taker once tried to test me."
+  ],
+
+  "terminator 2: judgment day": [
+    "Hasta la vista, baby.",
+    "Come with me if you want to live.",
+    "I'll be back."
+  ],
+
+  "a league of their own": [
+    "There's no crying in baseball!"
+  ],
+
+  "reservoir dogs": [
+    "Are you gonna bark all day, little doggie, or are you gonna bite?"
+  ],
+
+  "a few good men": [
+    "You can't handle the truth!",
+    "I want the truth!",
+    "You want answers?"
+  ],
+
+  "groundhog day": [
+    "Well, what if there is no tomorrow?",
+    "Don't drive angry!"
+  ],
+
+  "jurassic park": [
+    "Life finds a way.",
+    "Hold onto your butts."
+  ],
+
+  "the fugitive": [
+    "I didn't kill my wife.",
+    "I don't care."
+  ],
+
+  "tombstone": [
+    "I'm your huckleberry.",
+    "You're a daisy if you do."
+  ],
+
+  "mrs. doubtfire": [
+    "Help is on the way, dear!",
+    "It was a run-by fruiting!",
+    "Carpe dentum. Seize the teeth."
+  ],
+
+  "forrest gump": [
+    "Life was like a box of chocolates.",
+    "Run, Forrest, run!",
+    "Stupid is as stupid does."
+  ],
+
+  "pulp fiction": [
+    "Royale with cheese.",
+    "Zed's dead, baby. Zed's dead."
+  ],
+
+  "the shawshank redemption": [
+    "Get busy living, or get busy dying.",
+    "Some birds aren't meant to be caged."
+  ],
+
+  "the lion king": [
+    "Hakuna Matata!",
+    "Remember who you are."
+  ],
+
+  "apollo 13": [
+    "Houston, we have a problem."
+  ],
+
+  "braveheart": [
+    "They may take our lives, but they'll never take our freedom!"
+  ],
+
+  "toy story": [
+    "To infinity and beyond!"
+  ],
+
+  "jerry maguire": [
+    "Show me the money!",
+    "You complete me.",
+    "You had me at hello.",
+    "Help me help you."
+  ],
+
+  "scream": [
+    "What's your favorite scary movie?",
+    "Movies don't create psychos. Movies make psychos more creative."
+  ],
+
+  "good will hunting": [
+    "How do you like them apples?",
+    "It's not your fault."
+  ],
+
+  "titanic": [
+    "I'm the king of the world!",
+    "I'll never let go."
+  ],
+
+  "the big lebowski": [
+    "The Dude abides.",
+    "That rug really tied the room together."
+  ],
+
+  "saving private ryan": [
+    "Earn this."
+  ],
+
+  "the truman show": [
+    "Good morning! And in case I don't see ya, good afternoon, good evening and good night!"
+  ],
+
+  "the waterboy": [
+    "Now that's what I call high-quality H2O."
+  ],
+
+  "the sixth sense": [
+    "I see dead people.",
+    "They don't know they're dead."
+  ],
+
+  "fight club": [
+    "The first rule of Fight Club is: you do not talk about Fight Club."
+  ],
+
+  "the matrix": [
+    "There is no spoon.",
+    "I know kung fu."
+  ],
+
+  "american pie": [
+    "This one time, at band camp..."
+  ]
+
+};
+
+export default QUOTES_90S;
