4ac3af4a00b6a3b10913a6542a159e08020a745d
reelwise-design <djcohen@hotmail.com>
2026-09-17 14:24:47 -0400

    Add 2010s iconic quotes to quotes-2010s.js

---
+++ data/quotes-2010s.js
@@ -0,0 +1,114 @@
+/*
+  ============================================================
+  REELWISE ICONIC QUOTE VAULT — 2010s
+  ============================================================
+*/
+
+const QUOTES_2010S = {
+
+  "inception": [
+    "You mustn't be afraid to dream a little bigger, darling.",
+    "An idea is like a virus."
+  ],
+
+  "the social network": [
+    "You don't get to 500 million friends without making a few enemies."
+  ],
+
+  "bridesmaids": [
+    "Help me, I'm poor."
+  ],
+
+  "the avengers": [
+    "We have a Hulk.",
+    "That's my secret, Captain. I'm always angry."
+  ],
+
+  "django unchained": [
+    "I like the way you die, boy."
+  ],
+
+  "silver linings playbook": [
+    "Excelsior."
+  ],
+
+  "the wolf of wall street": [
+    "Sell me this pen.",
+    "I'm not leaving."
+  ],
+
+  "frozen": [
+    "Let it go!"
+  ],
+
+  "guardians of the galaxy": [
+    "We are Groot."
+  ],
+
+  "interstellar": [
+    "Love is the one thing we're capable of perceiving that transcends dimensions of time and space."
+  ],
+
+  "whiplash": [
+    "Not quite my tempo."
+  ],
+
+  "john wick": [
+    "People keep asking if I'm back."
+  ],
+
+  "mad max: fury road": [
+    "What a lovely day!"
+  ],
+
+  "the revenant": [
+    "As long as you can still grab a breath, you fight."
+  ],
+
+  "deadpool": [
+    "Maximum effort."
+  ],
+
+  "la la land": [
+    "Here's to the fools who dream."
+  ],
+
+  "hidden figures": [
+    "Every time we get a chance to get ahead, they move the finish line."
+  ],
+
+  "get out": [
+    "Get out!"
+  ],
+
+  "wonder woman": [
+    "It's not about deserve. It's about what you believe."
+  ],
+
+  "avengers: infinity war": [
+    "I am inevitable."
+  ],
+
+  "a star is born": [
+    "I just wanted to take another look at you."
+  ],
+
+  "bohemian rhapsody": [
+    "We're four misfits who don't belong together."
+  ],
+
+  "joker": [
+    "Why so serious?"
+  ],
+
+  "once upon a time... in hollywood": [
+    "I'm the devil. And I'm here to do the devil's business."
+  ],
+
+  "ford v ferrari": [
+    "There's a point at 7,000 RPM where everything fades."
+  ]
+
+};
+
+export default QUOTES_2010S;
