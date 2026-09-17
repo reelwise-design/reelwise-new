dc66ba4445fcd1e8ca7f76ee99fd4ccbfd15a4ca
reelwise-design <djcohen@hotmail.com>
2026-09-17 14:25:52 -0400

    Add classic movie quotes to quotes-classics.js

---
+++ data/quotes-classics.js
@@ -0,0 +1,178 @@
+/*
+  ============================================================
+  REELWISE ICONIC QUOTE VAULT — CLASSICS / PRE-1980
+  ============================================================
+*/
+
+const QUOTES_CLASSICS = {
+
+  "the wizard of oz": [
+    "There's no place like home.",
+    "I'll get you, my pretty!",
+    "Pay no attention to that man behind the curtain!"
+  ],
+
+  "gone with the wind": [
+    "Frankly, my dear, I don't give a damn.",
+    "After all, tomorrow is another day!"
+  ],
+
+  "casablanca": [
+    "Here's looking at you, kid.",
+    "We'll always have Paris.",
+    "Round up the usual suspects."
+  ],
+
+  "citizen kane": [
+    "Rosebud."
+  ],
+
+  "it's a wonderful life": [
+    "Every time a bell rings, an angel gets his wings.",
+    "No man is a failure who has friends.",
+    "To my big brother George, the richest man in town."
+  ],
+
+  "sunset boulevard": [
+    "All right, Mr. DeMille, I'm ready for my close-up."
+  ],
+
+  "a streetcar named desire": [
+    "Stella!"
+  ],
+
+  "singin' in the rain": [
+    "What a glorious feeling. I'm happy again."
+  ],
+
+  "on the waterfront": [
+    "I coulda been a contender."
+  ],
+
+  "the seven year itch": [
+    "Hey, did you ever try dunking a potato chip in champagne?"
+  ],
+
+  "the ten commandments": [
+    "Let my people go!"
+  ],
+
+  "some like it hot": [
+    "Well, nobody's perfect."
+  ],
+
+  "psycho": [
+    "A boy's best friend is his mother.",
+    "We all go a little mad sometimes."
+  ],
+
+  "spartacus": [
+    "I'm Spartacus!"
+  ],
+
+  "dr. no": [
+    "Bond. James Bond."
+  ],
+
+  "dr. strangelove": [
+    "Gentlemen, you can't fight in here! This is the War Room!"
+  ],
+
+  "goldfinger": [
+    "No, Mr. Bond, I expect you to die!"
+  ],
+
+  "the graduate": [
+    "Plastics.",
+    "Mrs. Robinson, you're trying to seduce me."
+  ],
+
+  "cool hand luke": [
+    "What we've got here is failure to communicate."
+  ],
+
+  "2001: a space odyssey": [
+    "I'm sorry, Dave. I'm afraid I can't do that.",
+    "Open the pod bay doors, HAL."
+  ],
+
+  "midnight cowboy": [
+    "I'm walkin' here!"
+  ],
+
+  "dirty harry": [
+    "You've got to ask yourself one question: Do I feel lucky?",
+    "Well, do ya, punk?"
+  ],
+
+  "the godfather": [
+    "I'm gonna make him an offer he can't refuse.",
+    "Leave the gun. Take the cannoli.",
+    "It's not personal, Sonny. It's strictly business."
+  ],
+
+  "the exorcist": [
+    "The power of Christ compels you!"
+  ],
+
+  "the godfather part ii": [
+    "Keep your friends close, but your enemies closer.",
+    "I know it was you, Fredo. You broke my heart."
+  ],
+
+  "jaws": [
+    "You're gonna need a bigger boat.",
+    "Smile, you son of a—"
+  ],
+
+  "monty python and the holy grail": [
+    "It's just a flesh wound.",
+    "Your mother was a hamster!"
+  ],
+
+  "taxi driver": [
+    "You talkin' to me?",
+    "Well, I'm the only one here."
+  ],
+
+  "rocky": [
+    "Yo, Adrian!",
+    "All I wanna do is go the distance.",
+    "Nobody's ever gone the distance with Creed."
+  ],
+
+  "star wars": [
+    "May the Force be with you.",
+    "Help me, Obi-Wan Kenobi. You're my only hope.",
+    "The Force will be with you. Always."
+  ],
+
+  "close encounters of the third kind": [
+    "This means something. This is important."
+  ],
+
+  "animal house": [
+    "Toga! Toga!",
+    "Was it over when the Germans bombed Pearl Harbor?"
+  ],
+
+  "superman": [
+    "You'll believe a man can fly."
+  ],
+
+  "rocky ii": [
+    "Yo, Adrian! I did it!",
+    "There's one thing I want you to do for me. Win."
+  ],
+
+  "alien": [
+    "In space, no one can hear you scream."
+  ],
+
+  "apocalypse now": [
+    "I love the smell of napalm in the morning."
+  ]
+
+};
+
+export default QUOTES_CLASSICS;
