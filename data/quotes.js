9effd1e011d6775d186c0faef6ae5780dbe2758d
reelwise-design <djcohen@hotmail.com>
2026-09-17 14:10:30 -0400

    Add iconic movie quotes to quotes.js
    This file contains a collection of iconic movie quotes curated by Reelwise, organized by movie title.

---
+++ data/quotes.js
@@ -0,0 +1,242 @@
+/*
+  ============================================================
+  REELWISE ICONIC QUOTE VAULT
+  ============================================================
+
+  This file contains Reelwise-curated iconic movie quotes.
+
+  Keep this file separate from the quote engine so the Vault
+  can grow without changing api/quotes.js.
+  ============================================================
+*/
+
+const QUOTE_VAULT = {
+
+  "the shawshank redemption": [
+    "Get busy living, or get busy dying.",
+    "Hope is a good thing, maybe the best of things, and no good thing ever dies.",
+    "These walls are funny. First you hate 'em, then you get used to 'em.",
+    "Some birds aren't meant to be caged. Their feathers are just too bright."
+  ],
+
+  "it's a wonderful life": [
+    "Every time a bell rings, an angel gets his wings.",
+    "No man is a failure who has friends.",
+    "I want to live again!",
+    "You want the moon? Just say the word and I'll throw a lasso around it and pull it down.",
+    "Merry Christmas, you wonderful old Building and Loan!",
+    "To my big brother George, the richest man in town."
+  ],
+
+  "rocky": [
+    "Yo, Adrian!",
+    "All I wanna do is go the distance.",
+    "Nobody's ever gone the distance with Creed.",
+    "It really don't matter if I lose this fight.",
+    "I just wanna prove somethin' — I ain't no bum."
+  ],
+
+  "rocky ii": [
+    "Yo, Adrian! I did it!",
+    "You're gonna eat lightning and you're gonna crap thunder!",
+    "There's one thing I want you to do for me. Win.",
+    "I just gotta be around you.",
+    "I never asked you to stop being a woman. Please don't ask me to stop being a man."
+  ],
+
+  "rocky iii": [
+    "I don't hate Balboa. I pity the fool.",
+    "There is no tomorrow!",
+    "You ain't so bad!",
+    "Nothing is real if you don't believe in who you are."
+  ],
+
+  "rocky iv": [
+    "I must break you.",
+    "If he dies, he dies.",
+    "He's not a machine. He's a man!",
+    "If I can change, and you can change, everybody can change!"
+  ],
+
+  "rocky balboa": [
+    "It ain't about how hard you hit.",
+    "It's about how hard you can get hit and keep moving forward.",
+    "That's how winning is done!",
+    "The world ain't all sunshine and rainbows.",
+    "You, me, or nobody is gonna hit as hard as life.",
+    "Until you start believing in yourself, you ain't gonna have a life."
+  ],
+
+  "jaws": [
+    "You're gonna need a bigger boat.",
+    "Smile, you son of a—",
+    "That's some bad hat, Harry.",
+    "Here's to swimmin' with bow-legged women."
+  ],
+
+  "top gun": [
+    "I feel the need — the need for speed!",
+    "You can be my wingman any time.",
+    "That's right, Iceman. I am dangerous.",
+    "Talk to me, Goose.",
+    "Your ego is writing checks your body can't cash."
+  ],
+
+  "top gun: maverick": [
+    "It's not the plane, it's the pilot.",
+    "Don't think. Just do.",
+    "Talk to me, Goose.",
+    "It's time to let go.",
+    "You'll know when I'm gone."
+  ],
+
+  "a few good men": [
+    "You can't handle the truth!",
+    "I want the truth!",
+    "You want answers?",
+    "You don't need to wear a patch on your arm to have honor."
+  ],
+
+  "back to the future": [
+    "Where we're going, we don't need roads.",
+    "Great Scott!",
+    "Nobody calls me chicken.",
+    "If my calculations are correct, when this baby hits 88 miles per hour, you're gonna see some serious stuff."
+  ],
+
+  "the godfather": [
+    "I'm gonna make him an offer he can't refuse.",
+    "Leave the gun. Take the cannoli.",
+    "It's not personal, Sonny. It's strictly business.",
+    "A man who doesn't spend time with his family can never be a real man."
+  ],
+
+  "the godfather part ii": [
+    "Keep your friends close, but your enemies closer.",
+    "I know it was you, Fredo. You broke my heart.",
+    "We're both part of the same hypocrisy, Senator.",
+    "If anything in this life is certain, if history has taught us anything, it is that you can kill anyone."
+  ],
+
+  "goodfellas": [
+    "As far back as I can remember, I always wanted to be a gangster.",
+    "Funny how? I mean, funny like I'm a clown?",
+    "Never rat on your friends, and always keep your mouth shut.",
+    "For us to live any other way was nuts.",
+    "Now go home and get your shine box."
+  ],
+
+  "the hangover": [
+    "What happens in Vegas stays in Vegas. Except herpes.",
+    "I'm not supposed to be within two hundred feet of a school.",
+    "You guys might not know this, but I consider myself a bit of a loner."
+  ],
+
+  "old school": [
+    "We're going streaking!",
+    "You're my boy, Blue!",
+    "Once it hits your lips, it's so good!",
+    "Just ring the bell, you pansy.",
+    "I have a nice little Saturday planned."
+  ],
+
+  "forrest gump": [
+    "Life was like a box of chocolates.",
+    "Run, Forrest, run!",
+    "Stupid is as stupid does.",
+    "I'm not a smart man, but I know what love is."
+  ],
+
+  "the sixth sense": [
+    "I see dead people.",
+    "They don't know they're dead.",
+    "They only see what they want to see."
+  ],
+
+  "fight club": [
+    "The first rule of Fight Club is: you do not talk about Fight Club.",
+    "The second rule of Fight Club is: you do not talk about Fight Club.",
+    "It's only after we've lost everything that we're free to do anything.",
+    "This is your life, and it's ending one minute at a time."
+  ],
+
+  "jerry maguire": [
+    "Show me the money!",
+    "You complete me.",
+    "You had me at hello.",
+    "Help me help you."
+  ],
+
+  "dirty harry": [
+    "You've got to ask yourself one question: Do I feel lucky?",
+    "Well, do ya, punk?"
+  ],
+
+  "taxi driver": [
+    "You talkin' to me?",
+    "Well, I'm the only one here.",
+    "Someday a real rain will come and wash all this scum off the streets."
+  ],
+
+  "scream": [
+    "What's your favorite scary movie?",
+    "Movies don't create psychos. Movies make psychos more creative.",
+    "There are certain rules that one must abide by in order to successfully survive a horror movie."
+  ],
+
+  "dirty dancing": [
+    "Nobody puts Baby in a corner.",
+    "I carried a watermelon.",
+    "I'm scared of walking out of this room and never feeling the rest of my whole life the way I feel when I'm with you."
+  ],
+
+  "the sandlot": [
+    "You're killing me, Smalls!",
+    "Heroes get remembered, but legends never die.",
+    "For-ev-er."
+  ],
+
+  "airplane!": [
+    "Surely you can't be serious.",
+    "I am serious. And don't call me Shirley.",
+    "Looks like I picked the wrong week to quit smoking.",
+    "Roger, Roger. What's our vector, Victor?"
+  ],
+
+  "groundhog day": [
+    "Well, what if there is no tomorrow? There wasn't one today.",
+    "Don't drive angry!",
+    "I'm a god. I'm not the God... I don't think."
+  ],
+
+  "when harry met sally...": [
+    "I'll have what she's having.",
+    "You made a woman meow?",
+    "Men and women can't be friends because the sex part always gets in the way."
+  ],
+
+  "rudy": [
+    "You're five-foot-nothin', a hundred-and-nothin'.",
+    "In this lifetime, you don't have to prove nothin' to nobody except yourself.",
+    "I've been ready for this my whole life."
+  ],
+
+  "mrs. doubtfire": [
+    "Help is on the way, dear!",
+    "It was a run-by fruiting!",
+    "Oh, sir. I saw it! Some angry member of the kitchen staff. Did you not tip them?",
+    "Carpe dentum. Seize the teeth.",
+    "I admire that honesty, Natalie. That's a noble quality. Never lose that, because it often disappears with age, or entering politics."
+  ],
+
+  "the departed": [
+    "I'm the guy who does his job. You must be the other guy.",
+    "When you're facing a loaded gun, what's the difference?",
+    "I don't want to be a product of my environment. I want my environment to be a product of me.",
+    "Maybe. Maybe not. Maybe fuck yourself.",
+    "One of us had to die. With me, it tends to be the other guy."
+  ]
+
+};
+
+export default QUOTE_VAULT;
