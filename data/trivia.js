280aecf490dff142e598af4a348b3659f865d77e
reelwise-design <djcohen@hotmail.com>
2026-09-17 15:34:59 -0400

    Add 2020s trivia to the trivia vault

--- data/trivia.js
+++ data/trivia.js
@@ -8,7 +8,7 @@
   Trivia Vault = PRIMARY source
   Wikipedia = FALLBACK source
 
-  To expand Reelwise, add trivia to the appropriate era file.
+  To add or correct trivia, edit the appropriate era file.
 */
 
 import TRIVIA_CLASSICS from "./trivia-classics.js";
@@ -16,26 +16,15 @@ import TRIVIA_80S from "./trivia-80s.js";
 import TRIVIA_90S from "./trivia-90s.js";
 import TRIVIA_2000S from "./trivia-2000s.js";
 import TRIVIA_2010S from "./trivia-2010s.js";
-
+import TRIVIA_2020S from "./trivia-2020s.js";
 
 const TRIVIA_VAULT = {
-
-  // Classic movies through the 1970s
   ...TRIVIA_CLASSICS,
-
-  // 1980–1989
   ...TRIVIA_80S,
-
-  // 1990–1999
   ...TRIVIA_90S,
-
-  // 2000–2009
   ...TRIVIA_2000S,
-
-  // 2010–2019
-  ...TRIVIA_2010S
-
+  ...TRIVIA_2010S,
+  ...TRIVIA_2020S
 };
-
 
 export default TRIVIA_VAULT;
